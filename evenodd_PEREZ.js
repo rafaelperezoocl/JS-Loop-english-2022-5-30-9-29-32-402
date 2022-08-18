67596b8650b5c43db7df2a8d006b976b15789ebc
PEREZRA <rafael.perez@oocl.com>
2022-08-18 16:27:32 +0800

    loops added

---
+++ evenodd_PEREZ.js
@@ -0,0 +1,15 @@
+for (var a=1; a<=20; a++) {
+
+    if (a % 2 === 0) {
+
+           console.log(a + " is even.\n");  
+
+    }
+
+    else {
+
+            console.log(a + " is odd.\n");
+
+    }
+
+}
---
+++ multiply_PEREZ.js
@@ -0,0 +1,9 @@
+for (var i = 1; i <= 9; i++) {
+    for(var x = 1; x <= i; x++){
+        process.stdout.write(i +" * "+ i +" = "+ (i*i)+ " ");
+
+
+
+   }
+    console.log();
+}
